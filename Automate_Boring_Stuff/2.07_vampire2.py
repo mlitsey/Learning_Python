725e2ce4fba28e659010c194a6bb508b726df157
Michael Litsey <michael.litsey@gmail.com>
2018-10-31 11:46:12 -0400

    First commit

---
+++ Automate_Boring_Stuff/2.07_vampire2.py
@@ -0,0 +1,15 @@
+print('What is your name?')
+name = input()
+print('How old are you?')
+age = input()
+if name == 'Alice':
+    print('Hi, Alice.')
+elif int(age) < 12:
+    print('You are not Alice, kiddo.')
+elif int(age) > 100:
+    print('You are not Alice, grannie.')
+elif int(age) > 2000:
+    print('Unlike you, Alice is not an undead, immortal vampire.')
+else:
+    print("I don't know you.")
+# added lines 1-4, changed elif age to elif int(age), added lines 13-14 --> michael.litsey@gmail.com
